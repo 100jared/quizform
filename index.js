44bdb97652fea13fe78097cc04ee405b5b2841cf
100jared <jerikorir@gmail.com>
2022-02-06 16:51:15 +0300

    refactor: adding a function onclick to confirm if you followed the instruction

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function check(){
-    var c = 0;
+    var c = 0; //c means correct answer
     var q1 = document.quiz.question1.value;
     var q2 = document.quiz.question2.value;
     var q3 = document.quiz.question3.value;
@@ -7,14 +7,14 @@ function check(){
     var q5 = document.quiz.question5.value;
     var result=document.getElementById('result');
     var quiz = document.getElementById('quiz')
-    if(q1 == "compiler"){c++}
-    if(q2 == "PHP"){c++}
-    if(q3 == "Syntax"){c++}
-    if(q4 == ".NET"){c++}
-    if(q5 == "IDE"){c++}
+    if(q1 == "B"){c++}
+    if(q2 == "B"){c++}
+    if(q3 == "C"){c++}
+    if(q4 == "C"){c++}
+    if(q5 == "C"){c++}
 
     quiz.style.display = "none";
-    var finalAnswer = c*20;
+    var finalAnswer = c*20; //multiply by 20 to get 100%
     if(finalAnswer >= 80){
         result.textContent = "Your Score is: " + finalAnswer +"%"+" EXCELLENT!";
     }
@@ -23,5 +23,8 @@ function check(){
     }
     else if(finalAnswer<50){
         result.textContent = "Your Score is: " + finalAnswer +"%"+" POORLY DONE! Repeat the TEST";
-    }
+    }     
 }
+function checkQuiz(){
+    alert("I believe you followed the Instructions!");
+ }
